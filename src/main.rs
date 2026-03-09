cf0917d74ca9e1d64400c2a952adbe406802d85b
yy <yubinux@yeah.net>
2026-03-09 18:49:03 +0800

    update1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    target_mac: String,
+    router_ip: String,
+    scan_interval: u64,
+    smtp_host: String,
+    smtp_pass: String,
+    offline_threshold: u64,
+    smtp_user: String,
+    smtp_port: u16,
+    recipients: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenvy::dotenv().ok();
+    let config = envy::from_env::<Config>().expect("请检查 .env 文件配置是否完整");
+
+    match envy::from_env::<Config>() {
+        Ok(config) => {
+            println!("配置加载成功！");
+            println!("正在监控设备: {}", config.target_mac);
+            println!("通知名单: {:?}", config.recipients);
+
+        }
+        Err(error) => {
+            eprintln!("配置映射失败: {:#?}", error);
+        }
+    }
+
+
 }
